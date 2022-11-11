67cde59fda6f576df11ffe923222b2006574a90d
Parsa Mohammadian <parsa2820@gmail.com>
2022-11-11 13:31:41 +0330

    Add night mode switch

--- app/src/main/java/ir/parsa2820/mycv/MainActivity.kt
+++ app/src/main/java/ir/parsa2820/mycv/MainActivity.kt
@@ -6,18 +6,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val callMeButton: Button = findViewById(R.id.button)
+        val callMeButton: Button = findViewById(R.id.callMeButton)
         callMeButton.setOnClickListener {
-            val toast: Toast = Toast.makeText(this, "Calling...", Toast.LENGTH_SHORT)
-            toast.show()
             val number: Uri = Uri.parse(R.string.number.toString())
+            val toast: Toast = Toast.makeText(this, number.toString(), Toast.LENGTH_SHORT)
+            toast.show()
             val intent = Intent(Intent.ACTION_DIAL, number)
             startActivity(intent)
+        }
+
+        val nightModeSwitch: SwitchMaterial = findViewById(R.id.nightModeSwitch)
+        nightModeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_YES
+            } else {
+                delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_NO
+            }
         }
     }
 }
